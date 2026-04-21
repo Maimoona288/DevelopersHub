0c1153586dba04d0d9d6ac50655cc3f5cf5b768a
Maimoona288 <maimoonashahbaz348@gmail.com>
2026-04-22 02:43:39 +1200

    Folder structure setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config(); // load env variables
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+
+// MongoDB connection using env
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Use PORT from env
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
